0bea40a6c9cf8a926655ed727d4c64b39788de6b
sang89vh <sang89vh@gmail.com>
2017-01-18 16:16:13 +0700

    go to git

--- Mybox/MyboxCore/src/main/java/com/myboxteam/core/exception/MBException.java
+++ Mybox/MyboxCore/src/main/java/com/myboxteam/core/exception/MBException.java
@@ -1,16 +1,16 @@
 package com.myboxteam.core.exception;
 import java.util.List;
 
-public class ReException extends BaseException {
+public class MBException extends BaseException {
 	
 	private static final long serialVersionUID = 8823356956725033191L;
 
-	public ReException(ErrorCodeEnum errorCodeEnum) {
+	public MBException(ErrorCodeEnum errorCodeEnum) {
 		super();
 		this.setErrorCode(errorCodeEnum);
 	}
 	
-	public ReException(ErrorCodeEnum errorCodeEnum,List<FieldError> fieldErrors) {
+	public MBException(ErrorCodeEnum errorCodeEnum,List<FieldError> fieldErrors) {
 		super();
 		this.setErrorCode(errorCodeEnum);
 		this.setFieldErrors(fieldErrors);
