899aa86b64f21daf832b2006565663fe6d6a8cc1
hieuvt <hieuvt2008@gmail.com>
2017-02-06 08:49:33 +0700

    news controller

--- Mybox/WebApp/src/main/java/com/myboxteam/realestate/controller/NewsController.java
+++ Mybox/WebApp/src/main/java/com/myboxteam/realestate/controller/NewsController.java
@@ -5,20 +5,29 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.io.FilenameUtils;
 import org.json.JSONArray;
+import org.parse4j.ParseException;
 import org.parse4j.ParseGeoPoint;
 import org.parse4j.ParseObject;
 import org.parse4j.ParseQuery;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.myboxteam.core.controller.MBBaseController;
 import com.myboxteam.core.utils.MBUtils;
 import com.myboxteam.core.utils.MapUtils;
+import com.myboxteam.realestate.form.NewsForm;
 
 @Controller
 @SessionAttributes("news")
@@ -33,15 +42,14 @@ public class NewsController extends MBBaseController{
 		mav.setViewName("news/grid");
 		return mav;
 	}
-	
-	@RequestMapping("/property")
+	@RequestMapping("/property/{newUrl}")
 	public ModelAndView property(ModelAndView mav, HttpServletRequest request,
-			HttpServletResponse response) throws Exception {
+			HttpServletResponse response,
+			@PathVariable("newUrl") String newUrl) throws Exception {
 		
-		String objId = request.getParameter("obj");
 		logger.info(MBUtils.convertDateToPrettyTime(MBUtils.getCurrentDate()));
 		ParseQuery<ParseObject> query = ParseQuery.getQuery("RePlace");
-		ParseObject news = query.get(objId);
+		ParseObject news = query.get(newUrl);
 		
 		Map<String,Object> data=news.getData();	
 		Object formDate =data.get("fromDate");
@@ -61,16 +69,9 @@ public class NewsController extends MBBaseController{
 		mav.addObject("latitude",lo.getLatitude());
 		mav.addObject("longitude",lo.getLongitude());
 		
-		mav.addObject("baseLat",request.getParameter("lat"));
-		mav.addObject("baseLng",request.getParameter("lng"));
-
-		System.out.println("search = " + request.getParameter("search"));
-		mav.addObject("search", request.getParameter("search"));
 		mav.setViewName("news/property");
 		return mav;
 	}
-	
-
 	
 	@RequestMapping("/dang-tin")
 	public ModelAndView create(ModelAndView mav, HttpServletRequest request,
@@ -80,5 +81,29 @@ public class NewsController extends MBBaseController{
 		return mav;
 	}
 	
+	@RequestMapping(value="/dang-tin",method=RequestMethod.POST)
+	public @ResponseBody NewsForm save(@ModelAttribute("news") NewsForm news) throws Exception {
+		
+		return news;
+	}
+	
+	@RequestMapping(value = "/uploadImages")
+	public @ResponseBody Map<String,Object> upload(
+			@RequestParam(value = "file", required = true) MultipartFile file) throws ParseException {
+		String name = file.getOriginalFilename();
+		String ext = FilenameUtils.getExtension(name);
+		// generate output file
+		String output = MBUtils.generateFileName(ext);
 
+		// save file to storage
+		MBUtils.saveFile(output, file);
+		
+		ParseObject image = new ParseObject("ReImages");
+		image.put("path", output);
+		image.save();
+		
+		return image.getData();
+	}
+	
+	
 }
